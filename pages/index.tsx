6137e3499125097bf94d70523068ebb2b758b15f
Mikail Arslan <mikailarz@outlook.com>
2022-07-26 12:58:29 +0200

    Adds prettier to linstagedrc.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image    from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         </h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
