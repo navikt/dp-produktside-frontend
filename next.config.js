3aa10c338a7ca0bafdd422965f02627e799d4291
Mikail Arslan <mikailarz@outlook.com>
2022-08-03 10:33:32 +0200

    Revert "Layout"

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/dagpenger",
-  productionBrowserSourceMaps: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "src")],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/pages/api/isready.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  res.statusCode = 200;
-  res.end();
-}
