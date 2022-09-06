ffdc953a8f23707e595c491c83c76563c64f1c65
Mikail Arslan <mikailarz@outlook.com>
2022-09-06 12:23:13 +0200

    Legg til CSP for å unngå CORS-feil i Preview-mode

---
+++ csp.js
@@ -0,0 +1,48 @@
+const csp = {
+  "default-src": ["'self'", "tjenester.nav.no", "appres.nav.no"],
+  "script-src": [
+    "'self'",
+    "'unsafe-inline'",
+    "'unsafe-eval'",
+    "appres.nav.no",
+    "static.hotjar.com",
+    "script.hotjar.com",
+    "*.psplugin.com",
+    "*.nav.no",
+    "in2.taskanalytics.com",
+  ],
+  "style-src": [
+    "'self'",
+    "blob:",
+    "*.nav.no",
+    "arbeid.dev.nav.no",
+    "appres.nav.no",
+    "'unsafe-inline'",
+    "fonts.googleapis.com",
+  ],
+  "connect-src": [
+    "'self'",
+    "*.nav.no",
+    "appres.nav.no",
+    "amplitude.nav.no/collect",
+    "*.psplugin.com",
+    "*.hotjar.com",
+    "*.vc.hotjar.com",
+    "*.vc.hotjar.io:*",
+    "*.surveystats.hotjar.io",
+    "api.puzzel.com",
+    "nav.boost.ai",
+    "rt6o382n.api.sanity.io",
+    "ta-survey-v2.herokuapp.com",
+    "in2.taskanalytics.com",
+  ],
+  "font-src": ["'self'", "data:", "*.psplugin.com", "*.hotjar.com", "fonts.gstatic.com"],
+  "frame-src": ["'self'", "vars.hotjar.com"],
+  "img-src": ["'self'", "*.hotjar.com", "*.nav.no", "data:", "localhost"],
+};
+
+const stringified = Object.entries(csp)
+  .map((entry) => `${entry[0]} ${entry[1].join(" ")}`)
+  .join("; ");
+
+module.exports = stringified;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const csp = require("./csp");
 const path = require("path");
 
 /** @type {import('next').NextConfig} */
@@ -9,8 +10,27 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src")],
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: csp,
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
+      {
+        source: "/",
+        destination: "/dagpenger",
+        permanent: true,
+        basePath: false,
+      },
       {
         source: "/dagpenger/(.*)",
         destination: "https://www.nav.no/404",
