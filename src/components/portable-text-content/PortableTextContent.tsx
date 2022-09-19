e2944f0c5de4b3282b432ff72424c11c56196250
Knut Magne Riise <knut.magne.riise@nav.no>
2022-09-19 15:06:20 +0200

    lagt til resolver at gtonok mark fra sanity

--- src/components/portable-text-content/PortableTextContent.tsx
+++ src/components/portable-text-content/PortableTextContent.tsx
@@ -1,11 +1,14 @@
 import { PortableText, PortableTextProps } from "@portabletext/react";
 import { DagpengerKalkulator } from "components/dagpenger-kalkulator/DagpengerKalkulator";
+import { GtoNOK } from "./marks/GtoNOK";
 
 export default function PortableTextContent({ value }: PortableTextProps) {
+  // @ts-ignore
   return (
     <PortableText
       value={value}
       components={{
+        marks: { GtoNOK: GtoNOK },
         types: {
           customComponent: DagpengerKalkulator,
         },
