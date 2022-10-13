83dcaaf4e2baef38f133f4fc27872ced7fe671f4
Knut Magne Riise <knut.magne.riise@nav.no>
2022-10-13 12:17:48 +0200

    kommentarer, da vi må finne en fiks for å llese miljø, kanskje sette det i en variabel

--- src/sanity/useSanityPreview.ts
+++ src/sanity/useSanityPreview.ts
@@ -10,6 +10,7 @@ export function useSanityPreview<Data>(initialData: Data, query: string, params?
   const [context, dispatch] = usePreviewContext();
   const enablePreview = router.query.preview === "true" || context.previewMode;
   const dataset = (router.query.dataset as string) || context.dataset;
+  //obs, funker ikke. kan ikke bruke hostname i prod deploy, og man har ikke tilgang til location såpass tidlig i contexten tror jeg
   const enableDevBanner = hostname().includes("ekstern.dev") || hostname().includes("localhost") || context.devBanner;
 
   useEffect(() => {
