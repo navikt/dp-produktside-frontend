30e5635273eba780e67e4f5931c9332c67b7e115
Knut Magne Riise <knut.magne.riise@nav.no>
2022-10-13 13:20:30 +0200

    heller sjekke om vi er i dev basert på sanityvariabel

--- src/sanity/useSanityPreview.ts
+++ src/sanity/useSanityPreview.ts
@@ -3,15 +3,13 @@ import { useRouter } from "next/router";
 import { useEffect, useMemo } from "react";
 import { usePreviewContext } from "components/preview-context/previewContext";
 import { sanityConfig } from "./client";
-import { hostname } from "os";
 
 export function useSanityPreview<Data>(initialData: Data, query: string, params?: Record<string, any>): Data {
   const router = useRouter();
   const [context, dispatch] = usePreviewContext();
   const enablePreview = router.query.preview === "true" || context.previewMode;
   const dataset = (router.query.dataset as string) || context.dataset;
-  //obs, funker ikke. kan ikke bruke hostname i prod deploy, og man har ikke tilgang til location såpass tidlig i contexten tror jeg
-  const enableDevBanner = hostname().includes("ekstern.dev") || hostname().includes("localhost") || context.devBanner;
+  const enableDevBanner = process.env.NEXT_PUBLIC_SANITY_DATASET == "development" || context.devBanner;
 
   useEffect(() => {
     enablePreview && dispatch({ previewMode: true, dataset: dataset, showDrafts: true });
