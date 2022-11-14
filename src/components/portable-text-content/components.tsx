0028549408f8acf2c270605bf2db001857e4933a
Mikail Arslan <mikailarz@outlook.com>
2022-11-14 14:30:45 +0100

    Legger til Button i PortableText

--- src/components/portable-text-content/components.tsx
+++ src/components/portable-text-content/components.tsx
@@ -1,7 +1,9 @@
-import { Accordion, ReadMore } from "@navikt/ds-react";
+import { Accordion, Button, ReadMore } from "@navikt/ds-react";
 import { PortableTextTypeComponent } from "@portabletext/react";
 import { DagpengerKalkulator } from "components/dagpenger-kalkulator/DagpengerKalkulator";
+import Link from "next/link";
 import styles from "./PortableTextContent.module.scss";
+import * as NavIcons from "@navikt/ds-icons";
 
 export const commonComponents: Record<string, PortableTextTypeComponent<any> | undefined> | undefined = {
   produktsideAccordion: ({ value }) => (
@@ -10,6 +12,32 @@ export const commonComponents: Record<string, PortableTextTypeComponent<any> | u
       <Accordion.Content className={styles.whiteSpacePreline}>{value.content}</Accordion.Content>
     </Accordion.Item>
   ),
+  produktsideButton: ({ value }) => {
+    // @ts-ignore
+    const Icon = NavIcons?.[value?.iconName];
+
+    const iconProps = Icon
+      ? {
+          icon: <Icon aria-hidden />,
+          iconPosition: value?.iconPosition,
+        }
+      : {};
+
+    return (
+      <Link href={value?.url} passHref>
+        <Button
+          className={styles.button}
+          as="a"
+          target={value?.targetBlank ? "_blank" : "_self"}
+          size={value?.size}
+          variant={value?.variant}
+          {...iconProps}
+        >
+          {value?.title}
+        </Button>
+      </Link>
+    );
+  },
   produktsideReadMore: ({ value }) => (
     <ReadMore className={styles.whiteSpacePreline} header={value.title} size={value.size}>
       {value.content}
