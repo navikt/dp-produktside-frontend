2454073328388d4270049a401851c4998508cfed
Mikail Arslan <mikailarz@outlook.com>
2022-11-15 13:55:01 +0100

    Legger til Button i PortableText (#55)
    * Legger til Button i PortableText
    
    Co-authored-by: Knut Magne Riise <knut.magne.riise@nav.no>

--- src/components/portable-text-content/components.tsx
+++ src/components/portable-text-content/components.tsx
@@ -1,7 +1,9 @@
-import { Accordion, ReadMore } from "@navikt/ds-react";
+import { Accordion, Button, ReadMore } from "@navikt/ds-react";
 import { PortableTextTypeComponent } from "@portabletext/react";
 import { DagpengerKalkulator } from "components/dagpenger-kalkulator/DagpengerKalkulator";
+import Link from "next/link";
 import styles from "./PortableTextContent.module.scss";
+import * as NavIcons from "@navikt/ds-icons";
 
 export const commonComponents: Record<string, PortableTextTypeComponent<any> | undefined> | undefined = {
   produktsideAccordion: ({ value }) => (
@@ -10,6 +12,31 @@ export const commonComponents: Record<string, PortableTextTypeComponent<any> | u
       <Accordion.Content className={styles.whiteSpacePreline}>{value.content}</Accordion.Content>
     </Accordion.Item>
   ),
+  produktsideButton: ({ value }) => {
+    // @ts-ignore
+    const Icon = NavIcons?.[value?.iconName];
+
+    const iconProps = Icon
+      ? {
+          icon: <Icon aria-hidden />,
+          iconPosition: value?.iconPosition,
+        }
+      : {};
+
+    return (
+      <Button
+        className={styles.button}
+        as="a"
+        target={value?.targetBlank ? "_blank" : "_self"}
+        size={value?.size}
+        variant={value?.variant}
+        {...iconProps}
+        href={value?.url}
+      >
+        {value?.title}
+      </Button>
+    );
+  },
   produktsideReadMore: ({ value }) => (
     <ReadMore className={styles.whiteSpacePreline} header={value.title} size={value.size}>
       {value.content}
