b778bf92dc85ba06598415295d14d604101552c8
Mikail Arslan <mikailarz@outlook.com>
2022-11-17 12:33:31 +0100

    Midlertidig løsning for scrollingnårinnholdet filtreres (#58)

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -1,3 +1,5 @@
+import { RefObject } from "react";
+
 const scrollTo = (position: number) => {
   window.scrollTo({
     behavior: "smooth",
@@ -12,5 +14,14 @@ export const smoothScrollToTarget = (targetId: string, offset = 0) => {
     const top = targetElement.getBoundingClientRect().top + window.scrollY;
     scrollTo(top - offset);
     targetElement.focus({ preventScroll: true });
+  }
+};
+
+type ValueOf<T> = T[keyof T];
+
+export const scrollIntoViewWithOffset = (ref: RefObject<ValueOf<HTMLElementTagNameMap>>, offset: number) => {
+  if (ref?.current) {
+    const topPosition = ref.current.getBoundingClientRect().top - document.body.getBoundingClientRect().top - offset;
+    scrollTo(topPosition);
   }
 };
