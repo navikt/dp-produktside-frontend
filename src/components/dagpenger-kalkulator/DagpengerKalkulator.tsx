24086c390a47645488db55b46fbdced3402f844e
Mikail Arslan <mikailarz@outlook.com>
2022-11-18 13:02:35 +0100

    wip kalkulator redesign

--- src/components/dagpenger-kalkulator/DagpengerKalkulator.tsx
+++ src/components/dagpenger-kalkulator/DagpengerKalkulator.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Alert, TextField } from "@navikt/ds-react";
+import { Alert, BodyShort, Button, Radio, RadioGroup, Table, TextField } from "@navikt/ds-react";
 import { useGrunnbelopContext } from "components/grunnbelop-context/grunnbelop-context";
 import { useState } from "react";
 import { PortableTextContent } from "components/portable-text-content/PortableTextContent";
@@ -13,6 +13,7 @@ interface ResultatProps {
 
 function Resultat({ grunnlag }: ResultatProps) {
   const { G } = useGrunnbelopContext();
+
   const { getCalculatorTextWithTextId } = useSanityContext();
 
   if (!grunnlag) {
@@ -29,44 +30,60 @@ function Resultat({ grunnlag }: ResultatProps) {
     );
   }
 
-  //const under3G = Math.min(grunnlag, 3 * G);
   const mellom0og6g = Math.max(0, Math.min(grunnlag, 6 * G));
-  const over6G = Math.max(0, grunnlag - 6 * G);
-
-  //const resultatUnder3G = under3G * 0.624;
   const resultatMellom0og6G = mellom0og6g * 0.624;
   const totalt = resultatMellom0og6G;
 
   return (
     <>
-      <table className={styles.resultTable}>
-        <tbody>
-          <tr>
-            <td>
-              <i>{getCalculatorTextWithTextId("mellom")}</i>
-            </td>
-            <td>{toKR(mellom0og6g)} x 62.4 %</td>
-            <td> {toKR(resultatMellom0og6G)}</td>
-          </tr>
-          {over6G > 0 && (
-            <tr>
-              <td>
-                <i>Inntekt over 6 G</i>
-              </td>
-              <td>{toKR(over6G)} x 0 %</td>
-              <td>{toKR(0)}</td>
-            </tr>
-          )}
-          <tr>
-            <td colSpan={2}>{getCalculatorTextWithTextId("tilsammen")}</td>
-            <td>{toKR(totalt)}</td>
-          </tr>
-          <tr>
-            <td colSpan={2}>{getCalculatorTextWithTextId("ukesats")}</td>
-            <td> {toKR(totalt / 52)}</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table zebraStripes>
+        <Table.Header>
+          <Table.Row shadeOnHover={false}>
+            <Table.HeaderCell scope="col">Beregningsgrunnlag</Table.HeaderCell>
+            <Table.HeaderCell scope="col"></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row shadeOnHover={false}>
+            <Table.DataCell>Din inntekt opptil 6G</Table.DataCell>
+            <Table.DataCell>{toKR(mellom0og6g)}</Table.DataCell>
+          </Table.Row>
+
+          <Table.Row shadeOnHover={false}>
+            <Table.DataCell>Antall barn</Table.DataCell>
+            <Table.DataCell>0</Table.DataCell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
+      <Table zebraStripes>
+        <Table.Header>
+          <Table.Row shadeOnHover={false}>
+            <Table.HeaderCell scope="col">Utregning</Table.HeaderCell>
+            <Table.HeaderCell scope="col"></Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row shadeOnHover={false}>
+            <Table.DataCell>62,4 % av inntekt opp til 6G</Table.DataCell>
+            <Table.DataCell>{toKR(resultatMellom0og6G)}</Table.DataCell>
+          </Table.Row>
+
+          <Table.Row shadeOnHover={false}>
+            <Table.DataCell>{getCalculatorTextWithTextId("ukesats")}</Table.DataCell>
+            <Table.DataCell>{toKR(totalt / 52)}</Table.DataCell>
+          </Table.Row>
+
+          <Table.Row shadeOnHover={false}>
+            <Table.DataCell>{getCalculatorTextWithTextId("tilsammen")}</Table.DataCell>
+            <Table.DataCell>{toKR(totalt)}</Table.DataCell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+
+      <BodyShort>Alle tall er før skatt.</BodyShort>
       <Alert variant="info">{getCalculatorTextWithTextId("kunveiledende")}</Alert>
     </>
   );
@@ -87,23 +104,29 @@ export function DagpengerKalkulator() {
   // }, [grunnlag, harLoggetBruk]);
 
   return (
-    <Accordion className={styles.kalkulator}>
-      <Accordion.Item open={true}>
-        <Accordion.Header>Kalkulator</Accordion.Header>
-        <Accordion.Content className={styles.inputWrapper}>
-          <TextField
-            label={getCalculatorTextWithTextId("label")}
-            type="text"
-            inputMode="numeric"
-            pattern="[0-9]*"
-            // @ts-ignore
-            value={grunnlag || ""}
-            onChange={(e) => setGrunnlag(Math.max(0, +e.target.value) || undefined)}
-            placeholder="350 000"
-          />
-        </Accordion.Content>
-        <Resultat grunnlag={debouncedGrunnlag} />
-      </Accordion.Item>
-    </Accordion>
+    <div className={styles.container}>
+      <TextField
+        label={getCalculatorTextWithTextId("label")}
+        type="text"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        // @ts-ignore
+        value={grunnlag || ""}
+        onChange={(e) => setGrunnlag(Math.max(0, +e.target.value) || undefined)}
+        placeholder="350 000"
+        size="medium"
+      />
+
+      <RadioGroup legend="Velg din aldersgruppe." onChange={(val: any) => setVal(val)} value={val}>
+        <Radio value="true">Ja</Radio>
+        <Radio value="false">Nei</Radio>
+      </RadioGroup>
+
+      <Button className={styles.button} variant="secondary">
+        Beregn hva jeg kan få
+      </Button>
+
+      <Resultat grunnlag={debouncedGrunnlag} />
+    </div>
   );
 }
