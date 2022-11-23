8436b27f153a4b116a61d20e4f6ef468e14490f7
Mikail Arslan <mikailarz@outlook.com>
2022-11-23 13:09:54 +0100

    wip kalkulator validation med react-hook-form

--- src/components/dagpenger-kalkulator/DagpengerKalkulator.tsx
+++ src/components/dagpenger-kalkulator/DagpengerKalkulator.tsx
@@ -1,24 +1,39 @@
 import { Button, Radio, RadioGroup, Select, TextField } from "@navikt/ds-react";
 import { useEffect, useRef, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 import styles from "./DagpengerKalkulator.module.scss";
 import { ResultTables } from "./ResultTables";
 
 function convertStringToBoolean(value?: string): boolean {
-  return value === "true" ? true : false;
+  return value === "true";
 }
 
-function convertBooleanToString(value?: boolean) {
-  return value ? "true" : "false";
+interface FormValues {
+  grunnlag: number;
+  hasChildren: string;
+  numberOfChildren: number;
 }
 
 export function DagpengerKalkulator() {
-  const [grunnlag, setGrunnlag] = useState<number | undefined>();
-  const [hasChildren, setHasChildren] = useState<boolean | undefined>(undefined);
-  const [numberOfChildren, setNumberOfChildren] = useState<number>(0);
-  const [showResult, setShowResult] = useState<boolean>();
-  const childrenOptions = Array.from({ length: 10 }, (_, i) => <option value={i + 1}>{i + 1}</option>);
-  const resultRef = useRef<HTMLDivElement | null>(null);
+  const [showResult, setShowResult] = useState<boolean>(false);
+  const resultTablesContainerRef = useRef<HTMLDivElement | null>(null);
+  const {
+    register,
+    control,
+    formState: { errors },
+    handleSubmit,
+    watch,
+  } = useForm<FormValues>();
+  const watchGrunnlag = watch("grunnlag");
+  const watchHasChildren = watch("hasChildren");
+  const watchNumberOfChildren = watch("numberOfChildren");
+  const hasChildren = convertStringToBoolean(watchHasChildren);
 
+  const childrenOptions = Array.from({ length: 10 }, (_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
   // TODO: Logg kalkulator bruk?
   // const [harLoggetBruk, setHarLoggetBruk] = useState(false);
   // useEffect(() => {
@@ -32,55 +47,53 @@ export function DagpengerKalkulator() {
     if (showResult) {
       setShowResult(false);
     }
-  }, [grunnlag, hasChildren, numberOfChildren]);
+  }, [watchGrunnlag, watchHasChildren, watchNumberOfChildren]);
+
+  function onSubmit() {
+    setShowResult(true);
+    resultTablesContainerRef?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+  }
 
   return (
-    <form
-      className={styles.container}
-      onSubmit={(event) => {
-        event.preventDefault();
-        setShowResult(true);
-        resultRef?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
-      }}
-    >
+    <form className={styles.container} onSubmit={handleSubmit(onSubmit)}>
       <TextField
+        {...register("grunnlag", { required: "Du må skrive inn inntekt" })}
         className={styles.textField}
         label="Hva har du hatt i inntekt de siste 12 månedene?"
         type="number"
         inputMode="numeric"
         pattern="[0-9]*"
-        value={grunnlag || ""}
-        onChange={(e) => {
-          setGrunnlag(Math.max(0, +e.target.value) || undefined);
-        }}
-        placeholder="700 000"
         size="medium"
-        required
+        error={errors?.grunnlag?.message as string}
       />
 
-      <RadioGroup
-        className={styles.radioGroup}
-        legend="Forsørger du barn under 18 år?"
-        onChange={(val: any) => {
-          if (val === "false") {
-            setNumberOfChildren(0);
-          }
-          setHasChildren(convertStringToBoolean(val));
-        }}
-        value={convertBooleanToString(hasChildren)}
-        required
-      >
-        <Radio value="true">Ja</Radio>
-        <Radio value="false">Nei</Radio>
-      </RadioGroup>
+      <Controller
+        control={control}
+        name="hasChildren"
+        rules={{ required: "Du må svare på om du forsørger barn under 18 år" }}
+        render={({ field: { value, onChange, onBlur, name, ref }, fieldState: { error } }) => (
+          <RadioGroup
+            ref={ref}
+            className={styles.radioGroup}
+            name={name}
+            legend="Forsørger du barn under 18 år?"
+            onChange={onChange}
+            onBlur={onBlur}
+            value={value}
+            error={error?.message}
+          >
+            <Radio value="true">Ja</Radio>
+            <Radio value="false">Nei</Radio>
+          </RadioGroup>
+        )}
+      />
 
       {hasChildren && (
         <Select
+          {...register("numberOfChildren", { required: "Du må velge antall barn", shouldUnregister: true })}
           className={styles.select}
           label="Hvor mange barn under 18 år forsørger du?"
-          value={numberOfChildren}
-          onChange={(event) => setNumberOfChildren(parseInt(event.target.value))}
-          required
+          error={errors?.numberOfChildren?.message as string}
         >
           <option value="">Velg antall barn under 18 år</option>
           {childrenOptions}
@@ -91,8 +104,8 @@ export function DagpengerKalkulator() {
         Beregn hva jeg kan få
       </Button>
 
-      <div ref={resultRef} className={styles.resultTablesContainer}>
-        {grunnlag && showResult && <ResultTables grunnlag={grunnlag} numberOfChildren={numberOfChildren} />}
+      <div ref={resultTablesContainerRef} className={styles.resultTablesContainer}>
+        {showResult && <ResultTables grunnlag={watchGrunnlag} numberOfChildren={watchNumberOfChildren ?? 0} />}
       </div>
     </form>
   );
