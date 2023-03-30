a58a89f027d2862e49e7f7de805b4df1b31cfc9b
Mikail Arslan <mikailarz@outlook.com>
2023-03-30 22:48:54 +0200

    Fikser URL for kopier lenke (#111)

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ const vars = {
 const appUrls = {
   nav404: "https://www.nav.no/404",
   dagpengerSoknad: "https://www.nav.no/arbeid/dagpenger/soknad-veileder",
-  produktsideAnchorUrl: (anchorId: string) => `https://www.nav.no/arbeid/dagpenger#${anchorId}`,
+  produktsideAnchorUrl: (anchorId: string) => `https://www.nav.no/dagpenger#${anchorId}`,
 };
 
 const basePath = "/dagpenger";
