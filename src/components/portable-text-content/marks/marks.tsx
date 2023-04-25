2b8586023833b2be6939a08b080dbadda80a4a67
Mikail Arslan <mikailarz@outlook.com>
2023-04-26 00:53:35 +0200

    Inliner lenker for slik at de får en mer naturlig flyt i tekst

--- src/components/portable-text-content/marks/marks.tsx
+++ src/components/portable-text-content/marks/marks.tsx
@@ -1,10 +1,15 @@
 import { Link } from "@navikt/ds-react";
 import { PortableTextMarkComponent } from "@portabletext/react";
 import { GtoNOK } from "./GtoNOK";
+import styles from "../PortableTextContent.module.scss";
 
 export const commonMarks: Record<string, PortableTextMarkComponent<any> | undefined> | undefined = {
   GtoNOK: GtoNOK,
   link: ({ children, value }) => {
-    return <Link href={value.href}>{children}</Link>;
+    return (
+      <Link href={value.href} className={styles.linkInline}>
+        {children}
+      </Link>
+    );
   },
 };
