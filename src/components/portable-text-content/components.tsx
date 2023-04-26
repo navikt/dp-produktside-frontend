a00d1748d64e22d0dfe118a98bcdd9f0e7034ab2
Mikail Arslan <mikailarz@outlook.com>
2023-04-26 20:39:36 +0200

    Legger amplitude logging for readmore i kalkulator

--- src/components/portable-text-content/components.tsx
+++ src/components/portable-text-content/components.tsx
@@ -5,6 +5,7 @@ import { MicroCards } from "components/card/MicroCard";
 import { DagpengerKalkulator } from "components/dagpenger-kalkulator/DagpengerKalkulator";
 import { LinkButton } from "components/link-button/LinkButton";
 import { ReadMoreWithRichText } from "components/readmore-with-rich-text/ReadMoreWithRichText";
+import { PortableTextContent } from "./PortableTextContent";
 
 export const commonComponents: Record<string, PortableTextTypeComponent<any> | undefined> | undefined = {
   produktsideAccordionWithRichText: AccordionWithRichText,
@@ -12,5 +13,9 @@ export const commonComponents: Record<string, PortableTextTypeComponent<any> | u
   produktsideButton: LinkButton,
   produktsideCalculator: DagpengerKalkulator,
   produktsideMicroCards: ({ value }) => <MicroCards title={value.title} cardList={value.cardList} />,
-  produktsideReadMoreWithRichText: ReadMoreWithRichText,
+  produktsideReadMoreWithRichText: ({ value }) => (
+    <ReadMoreWithRichText header={value.title} size={value.size} _key={value._key}>
+      <PortableTextContent value={value.content} />
+    </ReadMoreWithRichText>
+  ),
 };
--- src/components/readmore-with-rich-text/ReadMoreWithRichText.tsx
+++ src/components/readmore-with-rich-text/ReadMoreWithRichText.tsx
@@ -1,28 +1,34 @@
 import { ReadMore } from "@navikt/ds-react";
-import { PortableText, PortableTextTypeComponentProps } from "@portabletext/react";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { AnalyticsEvents, logAmplitudeEvent } from "utils/amplitude";
 import styles from "./ReadMoreWithRichText.module.scss";
 
-export function ReadMoreWithRichText({ value }: PortableTextTypeComponentProps<any>) {
+interface ReadMoreWithRichTextProps {
+  _key: string;
+  header: string;
+  children: ReactNode;
+  size?: "small" | "medium";
+}
+
+export function ReadMoreWithRichText({ _key, header, children, size = "medium" }: ReadMoreWithRichTextProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <ReadMore
       onClick={() => {
         logAmplitudeEvent(isOpen ? AnalyticsEvents.ACC_COLLAPSE : AnalyticsEvents.ACC_EXPAND, {
-          _key: value._key,
-          tekst: value.title,
+          _key: _key,
+          tekst: header,
           komponent: "ReadMore",
         });
         setIsOpen((prevIsOpen) => !prevIsOpen);
       }}
       open={isOpen}
       className={styles.readMore}
-      header={value.title}
-      size={value.size}
+      header={header}
+      size={size}
     >
-      <PortableText value={value.content} />
+      {children}
     </ReadMore>
   );
 }
