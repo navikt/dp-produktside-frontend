7eacecf2db1d158c772f0d21900b11c6c6e7e518
Mikail Arslan <mikailarz@outlook.com>
2023-06-13 13:16:55 +0200

    Fjerner søknadsknappen i venstremeny

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,6 +1,5 @@
 export const appUrls = {
   nav404: "https://www.nav.no/404",
-  dagpengerSoknad: "https://www.nav.no/dagpenger/dialog/soknad/",
   produktsideProductionUrl: (anchorId: string) => `https://www.nav.no/dagpenger#${anchorId}`,
   produktsidePublicUrl: (href: string) => `${process.env.APP_ORIGIN_URL}${href}`,
 };
