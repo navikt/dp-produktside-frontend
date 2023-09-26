18256393d2f26603b79de7262aafd612ed5bd36f
Mikail Arslan <mikailarz@outlook.com>
2023-09-26 14:16:27 +0200

    Fix/accesibility (#177)
    * Kalkulator forbedringer
    
    * Fiks kalkulator select bredde

--- src/components/dagpenger-kalkulator/utils.ts
+++ src/components/dagpenger-kalkulator/utils.ts
@@ -1,6 +1,6 @@
 export function toKR(kr: number, locale?: string) {
   if (locale === "en") {
-    return "NOK " + Math.round(kr).toLocaleString("nb-NO");
+    return Math.round(kr).toLocaleString("nb-NO") + " NOK";
   }
 
   return Math.round(kr).toLocaleString("nb-NO") + " kr";
