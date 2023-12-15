eff3a4896edf56a3006b71e1609ba7f0850db5c6
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2023-12-15 14:46:41 +0100

    Åpenbar kode

--- src/components/dagpenger-kalkulator/utils.ts
+++ src/components/dagpenger-kalkulator/utils.ts
@@ -8,16 +8,18 @@ export function toKR(kr: number, locale?: string) {
   return Math.round(kr).toLocaleString("nb-NO") + " kr";
 }
 
-export function getMonthsToSubtract(date: Date): number {
+export function getMonthsToSubtract(dateObject: Date): number {
   const [sunday, monday, saturday] = [0, 1, 6];
+  const day = dateObject.getDay();
+  const date = dateObject.getDate();
 
   const rules = [
-    date.getDate() <= 5,
-    date.getDate() === 5 && date.getDay() === saturday,
-    date.getDate() === 5 && date.getDay() === sunday,
-    date.getDate() === 6 && date.getDay() === sunday,
-    date.getDate() === 6 && date.getDay() === monday,
-    date.getDate() === 7 && date.getDay() === monday,
+    date <= 5,
+    date === 5 && day === saturday,
+    date === 5 && day === sunday,
+    date === 6 && day === sunday,
+    date === 6 && day === monday,
+    date === 7 && day === monday,
   ];
 
   if (rules.includes(true)) {
