43eb72bfaaad21013b5c941d5838d1af8486b867
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-01-08 09:22:56 +0100

    Revert sanity datasett config

--- src/sanity-utils/client.ts
+++ src/sanity-utils/client.ts
@@ -6,8 +6,7 @@ const projectId = "rt6o382n";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "development";
 
 export const sanityConfig = {
-  // Husk å revert denne før merge
-  dataset: "development",
+  dataset: dataset,
   projectId: projectId,
   useCdn: process.env.NODE_ENV === "production",
   apiVersion: "2021-06-06",
