be5c9789b0ac02643d729e445f8e98325eb0e511
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2024-04-16 12:48:21 +0200

    Legge til dagpenger link og page meta for SEO

--- src/pages/kalkulator/index.tsx
+++ src/pages/kalkulator/index.tsx
@@ -1,4 +1,5 @@
-import { BodyShort, Heading } from "@navikt/ds-react";
+import { BodyShort, Heading, Link } from "@navikt/ds-react";
+import { PageMeta } from "components/PageMeta";
 import { PortableTextContent } from "components/portable-text-content/PortableTextContent";
 import { GrunnbelopData } from "contexts/grunnbelop-context/GrunnbelopContext";
 import { useSanityContext } from "contexts/sanity-context/SanityContext";
@@ -6,7 +7,8 @@ import { GetStaticPropsContext } from "next";
 import Image from "next/image";
 import { sanityClient } from "sanity-utils/client";
 import { produktsideQuery } from "sanity-utils/groq/produktside/produktsideQuery";
-import svgIcon from "../../../public/kalkulator.svg";
+import kalkulatorIcon from "../../../public/kalkulator.svg";
+import dagpengerIcon from "../../../public/static-dagpenger.svg";
 import styles from "./styles.module.css";
 
 export async function getStaticProps({ locale }: GetStaticPropsContext) {
@@ -26,26 +28,33 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
 export default function KalkulatorIndex() {
   const sanityData = useSanityContext();
   /* @ts-ignore */
-  const { content, title, subtitle } = sanityData?.calculatorPage;
+  const { content, title, subtitle, dagpengerLink, seo } = sanityData?.calculatorPage;
 
   return (
-    <main>
-      <div className={styles.layout}>
-        <div className={styles.content}>
-          <div className={styles.header}>
-            <BodyShort className={styles.subtitle} aria-hidden>
-              {subtitle}
-            </BodyShort>
-            <div className={styles.title}>
-              <Heading size="large" level="1">
-                {title}
-              </Heading>
-              <Image src={svgIcon} aria-hidden alt="" />
+    <div>
+      <PageMeta title={seo?.title} description={seo?.description} seoImage={seo?.image} />
+      <main>
+        <div className={styles.layout}>
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <BodyShort className={styles.subtitle} aria-hidden>
+                {subtitle}
+              </BodyShort>
+              <div className={styles.title}>
+                <Heading size="large" level="1">
+                  {title}
+                </Heading>
+                <Image src={kalkulatorIcon} aria-hidden alt="" />
+              </div>
             </div>
+            <PortableTextContent value={content} />
+            <Link className={styles.dagpengerLink} href={dagpengerLink.link}>
+              <Image src={dagpengerIcon} height={50} alt="Ikon for dagpenger" aria-hidden />
+              {dagpengerLink.text}
+            </Link>
           </div>
-          <PortableTextContent value={content} />
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
